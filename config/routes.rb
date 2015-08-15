cf43032c7c290385d6b8446d409c9cedd9e4a214
LiamEderzeel <liamederzeel@gmail.com>
2015-08-15 15:46:34 +0200

    version built in version controlle

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  # Api definition
-  namespace :api, default: { format: :jason}, constraints: { subdomain: 'api'}, path: '/' do
-    # We are going to list our resources here
-  end
+    # Api definition
+    namespace :api, default: { format: :jason},
+        constraints: { subdomain: 'api'}, path: '/' do
+
+        scope module: :v1, 
+            constraints: ApiConstraints.new(version: 1, default: true) do
+            # We are going to list our resources here
+        end
+    end
 end
