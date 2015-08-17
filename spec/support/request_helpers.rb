bfe17159c139869ddd65477b77b918db905cc032
LiamEderzeel <liamederzeel@gmail.com>
2015-08-17 23:11:25 +0200

    Adds user authentication token

--- spec/support/request_helpers.rb
+++ spec/support/request_helpers.rb
@@ -1,20 +1,24 @@
 module Request
-	module JsonHelpers
-		def json_response
-			@json_response ||= JSON.parse(response.body, symbolize_names: true)
-		end
-	end
-	module HeaderHelpers
-		def api_header(version = 1)
-			request.headers['Accept'] = "application/vnd.deadlines.v#{version}"
-		end
-		def api_respons_format = (Mime::JSON)
-			request.headers['Accept'] = "#{reques.headers[Accept]},#{format}"
-			request.headers[Content-Type] = format.to_s
-		end
-		def incude_default_accept_headers
-			api_header
-			api_respons_format
-		end
-	end
+  module JsonHelpers
+    def json_response
+      @json_response ||= JSON.parse(response.body, symbolize_names: true)
+    end
+  end
+
+  # Our headers helpers module
+  module HeadersHelpers
+    def api_header(version = 1)
+      request.headers['Accept'] = "application/vnd.deadlines.v#{version}"
+    end
+
+    def api_response_format(format = Mime::JSON)
+      request.headers['Accept'] = "#{request.headers['Accept']},#{format}"
+      request.headers['Content-Type'] = format.to_s
+    end
+
+    def include_default_accept_headers
+      api_header
+      api_response_format
+    end
+  end
 end
