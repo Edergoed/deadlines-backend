bee8ffa443d7be1c19667e0dc4aa543ce4ec3ffb
LiamEderzeel <liamederzeel@gmail.com>
2015-08-18 19:21:21 +0200

    small deprication fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# require 'factory_girl'
+# Dir[Rails.root.join("spec/factories/*.rb")].each {|f| require f}
+
+# 1.times { FactoryGirl.create :product }
+Product.delete_all
+Product.create(:id => 1, :title => 'lol', :published => 1, :user_id => 1)
+Product.create(:id => 2, :title => 'lol', :published => 1, :user_id => 1)
+Product.create(:id => 3, :title => 'lol', :published => 1, :user_id => 1)
+Product.create(:id => 4, :title => 'lol', :published => 1, :user_id => 1)
