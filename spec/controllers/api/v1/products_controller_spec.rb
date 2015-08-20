4bb5e0850f56bd7ee36e4600fc59c8efa69f0b0e
LiamEderzeel <liamederzeel@gmail.com>
2015-08-21 00:50:59 +0200

    Embeds the products_ids into the user serialiser and fetches the correct products from the index action endpoint

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Api::V1::ProductsController do
+
     describe "GET #show" do
-        before(:each) do
+        before(:each) do 
             @product = FactoryGirl.create :product
             get :show, id: @product.id
         end
@@ -19,25 +20,48 @@ describe Api::V1::ProductsController do
 
         it { should respond_with 200 }
     end
+
     describe "GET #index" do
         before(:each) do
-            4.times { FactoryGirl.create :product }
-            get :index
+            4.times { FactoryGirl.create :product } 
         end
 
-        it "returns 4 records from the database" do
-            products_response = json_response
-            expect(products_response[:products].length).to eq(4)
+        context "when is not receiving any product_ids parameter" do
+            before(:each) do
+                get :index
+            end
+
+            it "returns 4 records from the database" do
+                products_response = json_response
+                expect(products_response[:products].length).to eq(4)
+            end
+
+            it "returns the user object into each product" do
+                products_response = json_response[:products]
+                products_response.each do |product_response|
+                    expect(product_response[:user]).to be_present
+                end
+            end
+
+            it { should respond_with 200 }
         end
 
-        it "returns the user object into each product" do
-            products_response = json_response[:products]
-            products_response.each do |product_response|
-                expect(product_response[:user]).to be_present
+        context "when product_ids parameter is sent" do
+            before(:each) do
+                @user = FactoryGirl.create :user
+                3.times { FactoryGirl.create :product, user: @user }
+                get :index, product_ids: @user.product_ids
+            end
+
+            it "returns just the products that belong to the user" do
+                products_response = json_response[:products]
+                products_response.each do |product_response|
+                    expect(product_response[:user][:email]).to eql @user.email
+                end
             end
         end
 
-        it { should respond_with 200 }
+
     end
 
     describe "POST #create" do
@@ -45,7 +69,7 @@ describe Api::V1::ProductsController do
             before(:each) do
                 user = FactoryGirl.create :user
                 @product_attributes = FactoryGirl.attributes_for :product
-                api_authorization_header user.auth_token
+                api_authorization_header user.auth_token 
                 post :create, { user_id: user.id, product: @product_attributes }
             end
 
@@ -54,78 +78,79 @@ describe Api::V1::ProductsController do
                 expect(product_response[:title]).to eql @product_attributes[:title]
             end
 
-
             it { should respond_with 201 }
         end
 
         context "when is not created" do
             before(:each) do
-                user = FactoryGirl.create :user
-                @invalid_product_attributes = { title: "Smart TV", price: "Twelve dollars" }
-                api_authorization_header user.auth_token
-                post :create, { user_id: user.id, product: @invalid_product_attributes }
-            end
+                user = FactoryGirl.create :user 
+@invalid_product_attributes = { title: "Smart TV", price: "Twelve dollars" } #notice I'm not including the email
+api_authorization_header user.auth_token 
+post :create, { user_id: user.id, product: @invalid_product_attributes }
+end
 
-            it "renders an errors json" do
-                product_response = json_response
-                expect(product_response).to have_key(:errors)
-            end
+it "renders an errors json" do
+    product_response = json_response
+    expect(product_response).to have_key(:errors)
+end
 
-            it "renders the json errors on whye the user could not be created" do
-                product_response = json_response
-                expect(product_response[:errors][:price]).to include "is not a number"
-            end
+it "renders the json errors on whye the user could not be created" do
+    product_response = json_response
+    expect(product_response[:errors][:price]).to include "is not a number"
+end
 
-            it { should respond_with 422 }
-        end
+it { should respond_with 422 }
+end
+end
+
+describe "PUT/PATCH #update" do
+    before(:each) do
+        @user = FactoryGirl.create :user
+        @product = FactoryGirl.create :product, user: @user
+        api_authorization_header @user.auth_token 
     end
-    describe "PUT/PATCH #update" do
+
+    context "when is successfully updated" do
         before(:each) do
-            @user = FactoryGirl.create :user
-            @product = FactoryGirl.create :product, user: @user
-            api_authorization_header @user.auth_token
+            patch :update, { user_id: @user.id, id: @product.id, product: { title: "An expensive TV" } }
         end
 
-        context "when is successfully updated" do
-            before(:each) do
-                patch :update, { user_id: @user.id, id: @product.id, product: { title: "An expensive TV" } }
-            end
-
-            it "renders the json representation for the updated user" do
-                product_response = json_response[:product]
-                expect(product_response[:title]).to eql "An expensive TV"
-            end
-
-            it { should respond_with 200 }
+        it "renders the json representation for the updated user" do
+            product_response = json_response[:product]
+            expect(product_response[:title]).to eql "An expensive TV"
         end
 
-        context "when is not updated" do
-            before(:each) do
-                patch :update, { user_id: @user.id, id: @product.id, product: { price: "two hundred" } }
-            end
-
-            it "renders an errors json" do
-                product_response = json_response
-                expect(product_response).to have_key(:errors)
-            end
-
-            it "renders the json errors on whye the user could not be created" do
-                product_response = json_response
-                expect(product_response[:errors][:price]).to include "is not a number"
-            end
-
-            it { should respond_with 422 }
-        end
+        it { should respond_with 200 }
     end
 
-    describe "DELETE #destroy" do
+    context "when is not updated" do
         before(:each) do
-            @user = FactoryGirl.create :user
-            @product = FactoryGirl.create :product, user: @user
-            api_authorization_header @user.auth_token 
-            delete :destroy, { user_id: @user.id, id: @product.id }
+            patch :update, { user_id: @user.id, id: @product.id, product: { price: "two hundred" } }
         end
 
-        it { should respond_with 204 }
+        it "renders an errors json" do
+            product_response = json_response
+            expect(product_response).to have_key(:errors)
+        end
+
+        it "renders the json errors on whye the user could not be created" do
+            product_response = json_response
+            expect(product_response[:errors][:price]).to include "is not a number"
+        end
+
+        it { should respond_with 422 }
     end
+end
+
+describe "DELETE #destroy" do
+    before(:each) do
+        @user = FactoryGirl.create :user
+        @product = FactoryGirl.create :product, user: @user
+        api_authorization_header @user.auth_token 
+        delete :destroy, { user_id: @user.id, id: @product.id }
+    end
+
+    it { should respond_with 204 }
+end
+
 end
