2032e792db96803da922f4047a4fe1786e1e3bef
LiamEderzeel <liamederzeel@gmail.com>
2015-08-20 23:42:52 +0200

    Embeds the user into the product json output

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -12,6 +12,11 @@ describe Api::V1::ProductsController do
             expect(product_response[:title]).to eql @product.title
         end
 
+        it "has the user as a embeded object" do
+            product_response = json_response[:product]
+            expect(product_response[:user][:email]).to eql @product.user.email
+        end
+
         it { should respond_with 200 }
     end
     describe "GET #index" do
@@ -25,8 +30,16 @@ describe Api::V1::ProductsController do
             expect(products_response[:products].length).to eq(4)
         end
 
+        it "returns the user object into each product" do
+            products_response = json_response[:products]
+            products_response.each do |product_response|
+                expect(product_response[:user]).to be_present
+            end
+        end
+
         it { should respond_with 200 }
     end
+
     describe "POST #create" do
         context "when is successfully created" do
             before(:each) do
