1848d3b830fa46f48f71bf166b9d9fd3b82cbbb5
LiamEderzeel <liamederzeel@gmail.com>
2015-09-19 01:44:42 +0200

    user id now working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,21 @@
-require 'auth_token'
 class ApplicationController < ActionController::Base
+  require 'auth_token'
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   respond_to :html, :json
   protect_from_forgery with: :null_session
   #include Authenticable
+  before_action :authenticate
+
+  def authenticate
+    begin
+      token = request.headers['Authorization'].split(' ').last
+      payload, header = AuthToken.valid?(token)
+      @current_user = payload['id']
+    rescue
+      render json: { error: 'Authorization header not valid'}, status: :unauthorized
+    end
+  end
   protected
 
     ##
@@ -12,9 +23,7 @@ class ApplicationController < ActionController::Base
     # any actions by ensuring the request is transmitting a
     # valid JWT.
     def verify_jwt_token
-      head :unauthorized if request.headers['Authorization'].nil? || !AuthToken.valid?(request.headers['Authorization'].split(' ').last)
-      token = request.headers['Authorization'].split(' ').last
-      payload, header = AuthToken.valid?(token)
-      @current_user = payload['user_id']
+      head :unauthorized if request.headers['Authorization'].nil? ||
+      !AuthToken.valid?(request.headers['Authorization'].split(' ').last)
     end
   end
