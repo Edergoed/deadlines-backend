53d9e9c2429667090883130e486639de713e6603
LiamEderzeel <liamederzeel@gmail.com>
2015-09-19 01:12:10 +0200

    small fixes to post deadlines(not working)

--- app/controllers/api/v1/deadlines_controller.rb
+++ app/controllers/api/v1/deadlines_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::DeadlinesController < ApplicationController
+	require 'auth_token'
 	before_action :verify_jwt_token, only: [:create, :update, :destroy]
 	respond_to :json
+
 
 	def index
 		deadlines = params[:deadline_ids].present? ? Deadline.find(params[:deadline_ids]) : Deadline.all
@@ -8,17 +10,17 @@ class Api::V1::DeadlinesController < ApplicationController
 	end
 
 	def show
-		respond_with Deadline.find(params[:id])   
+		respond_with Deadline.find(params[:id])
 	end
 
 	def create
 		#this fixes a lot
-		deadline = Deadline.new(deadline_params.merge(creator_id: current_user.id))
-		# deadline = current_user.deadlines.build(deadline_params) 
+		deadline = Deadline.new(deadline_params.merge(creator_id: @current_user))
+		# deadline = current_user.deadlines.build(deadline_params)
 		if deadline.save
-			render json: deadline, status: 201, location: [:api, deadline] 
+			render json: deadline, status: 201, location: [:api, deadline]
 		else
-			render json: { errors: deadline.errors }, status: 422
+			render json: { errors: deadline.errors, plz: @current_user }, status: 422
 		end
 	end
 
@@ -26,7 +28,7 @@ class Api::V1::DeadlinesController < ApplicationController
 		# deadline = current_user.deadlines.find(params[:id])
 		# deadline = deadlines.find(params[:id])
 		deadline = Deadline.find(params[:id])
-		if deadline.update(deadline_params.merge(editor_id: current_user.id))
+		if deadline.update(deadline_params.merge(editor_id: @current_user))
 			render json: deadline, status: 200, location: [:api, deadline]
 		else
 			render json: { errors: deadline.errors }, status: 422
@@ -34,7 +36,7 @@ class Api::V1::DeadlinesController < ApplicationController
 	end
 
 	def destroy
-		# deadline = current_user.deadlines.find(params[:id]) 
+		# deadline = current_user.deadlines.find(params[:id])
 		deadline = Deadline.find(params[:id])
 		deadline.destroy
 		head 204
