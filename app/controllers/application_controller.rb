2c5c853222100c986ce13778e58d8ae8b93b2c32
LiamEderzeel <liamederzeel@gmail.com>
2015-09-19 01:12:10 +0200

    small fixes to post deadlines(not working)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,20 @@
+require 'auth_token'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   respond_to :html, :json
   protect_from_forgery with: :null_session
   #include Authenticable
-   protected
+  protected
 
     ##
     # This method can be used as a before filter to protect
     # any actions by ensuring the request is transmitting a
     # valid JWT.
     def verify_jwt_token
-      head :unauthorized if request.headers['Authorization'].nil? ||
-          !AuthToken.valid?(request.headers['Authorization'].split(' ').last)
+      head :unauthorized if request.headers['Authorization'].nil? || !AuthToken.valid?(request.headers['Authorization'].split(' ').last)
+      token = request.headers['Authorization'].split(' ').last
+      payload, header = AuthToken.valid?(token)
+      @current_user = payload['user_id']
     end
-end
+  end
