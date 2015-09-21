32e692c57a9c5775aefb22a8a422c399117a7765
LiamEderzeel <liamederzeel@gmail.com>
2015-09-21 20:28:19 +0200

    small changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   respond_to :html, :json
   protect_from_forgery with: :null_session
   #include Authenticable
-  before_action :authenticate
+  
 
   def authenticate
     begin
