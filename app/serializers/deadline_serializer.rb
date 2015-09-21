8f609f4f36013cf2fcbf10f2166bc7f9c2b60755
LiamEderzeel <liamederzeel@gmail.com>
2015-09-22 00:00:03 +0200

    deadlines new and edit revector

--- app/serializers/deadline_serializer.rb
+++ app/serializers/deadline_serializer.rb
@@ -1,6 +1,6 @@
 class DeadlineSerializer < ActiveModel::Serializer
 	#embed :ids, include: true
-	attributes :id, :title, :subject, :deadline, :class_id, :group_id, :content, :published, :creator, :editor
+	attributes :id, :title, :subject, :deadlineDateTime, :class_id, :group_id, :content, :published, :creator, :editor
 	# has_one :user, :key => :creator, embed: :objects, serializer: ShortUserSerializer
 	# def creator
 	# 	ShortUserSerializer.new(object.user, root: false)
