d2c04ea2a427482def536ac3519e02c20d7d05c6
LiamEderzeel <liamederzeel@gmail.com>
2015-09-22 23:40:56 +0200

    db create migration2

--- db/migrate/20150821202324_add_and_edit_product.rb
+++
@@ -1,8 +0,0 @@
-class AddAndEditDeadline < ActiveRecord::Migration
-	def change
-		rename_column :deadlines, :user_id, :creator_id
-		rename_index :deadlines, :user_id, :creator_id
-		add_column :deadlines, :editor_id, :integer
-		add_index :deadlines, :editor_id
-	end
-end
