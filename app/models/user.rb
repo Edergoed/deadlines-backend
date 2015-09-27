32a738a6fe76ec8290107999ec78f4b59355af85
LiamEderzeel <liamederzeel@gmail.com>
2015-09-28 00:01:26 +0200

    fixed signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	validates :auth_token, uniqueness: true
+	#validates :auth_token, uniqueness: true
 	# Include default devise modules. Others available are:
 	# :confirmable, :lockable, :timeoutable and :omniauthable
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
@@ -13,9 +13,9 @@ class User < ActiveRecord::Base
 	has_many :created_deadlines,  :foreign_key=>"creator_id", :class_name => "Deadline"
 	has_many :edited_deadlines,  :foreign_key=>"editor_id", :class_name => "Deadline"
 
-	def generate_authentication_token!
-		begin
-			self.auth_token = Devise.friendly_token
-		end while self.class.exists?(auth_token: auth_token)
-	end
+	#def generate_authentication_token!
+	#	begin
+	#		self.auth_token = Devise.friendly_token
+	#	end while self.class.exists?(auth_token: auth_token)
+	#end
 end
