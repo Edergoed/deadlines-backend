f09ad6c3f64c44827f62df5fbeb1ec2fccff539f
LiamEderzeel <liamederzeel@gmail.com>
2015-09-28 01:05:37 +0200

    gravatar

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
 
   require 'auth_token'
+  require 'digest/md5'
   respond_to :json
 
   def create 
@@ -9,7 +10,9 @@ class Api::V1::SessionsController < ApplicationController
     user = user_email.present? && User.find_by(email: user_email) 
 
     if user.valid_password? user_password
-      token = AuthToken.issue_token({ id: user.id, email: user.email})
+      hash = Digest::MD5.hexdigest(user.email)
+      gravatarUrl = "http://www.gravatar.com/avatar/#{hash}"
+      token = AuthToken.issue_token({ id: user.id, email: user.email, gravatar: gravatarUrl })
       render json: {:token => token}, status: 200, location: [:api, user]
     else
       render json: { errors: "Invalid email or password" }, status: 422
@@ -23,4 +26,3 @@ class Api::V1::SessionsController < ApplicationController
     head 204
   end
 end
-
