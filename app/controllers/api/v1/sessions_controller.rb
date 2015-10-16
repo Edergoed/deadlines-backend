51a2c4d81da9ef5f328002d030889857f5e35973
LiamEderzeel <liamederzeel@gmail.com>
2015-10-16 11:56:53 +0200

    small name in token fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,32 +1,40 @@
 class Api::V1::SessionsController < ApplicationController
 
-  require 'auth_token'
-  require 'digest/md5'
-  respond_to :json
+    require 'auth_token'
+    require 'digest/md5'
+    respond_to :json
 
-  def create
-    user_password = params[:session][:password]
-    user_email = params[:session][:email]
-    user = user_email.present? && User.find_by(email: user_email)
+    def create
+        user_password = params[:session][:password]
+        user_email = params[:session][:email]
+        user = user_email.present? && User.find_by(email: user_email)
 
-    if user.valid_password? user_password
-      if user.active
-        gravatarHash = Digest::MD5.hexdigest(user.email)
-        permissions = []
-        token = AuthToken.issue_token({ id: user.id, email: user.email, gravatar: gravatarHash })
-        render json: {:token => token}, status: 200, location: [:api, user]
-      else
-        render json: { errors: "Acount is disabled" }, status: 422
-      end
-    else
-      render json: { errors: "Invalid email or password" }, status: 422
+        if user != nil
+            if user.valid_password? user_password
+                if user.active
+                    gravatarHash = Digest::MD5.hexdigest(user.email)
+                    permissions = []
+                    if(user.prefix == nil)
+                    token = AuthToken.issue_token({ id: user.id, firstname: user.firstname, lastname: user.lastname, email: user.email, gravatar: gravatarHash })
+                    else
+                    token = AuthToken.issue_token({ id: user.id, firstname: user.firstname, prefix: user.prefix, lastname: user.lastname, email: user.email, gravatar: gravatarHash })
+                    end
+                    render json: {:token => token}, status: 200, location: [:api, user]
+                else
+                    render json: { errors: "Acount is disabled" }, status: 422
+                end
+            else
+                render json: { errors: "Invalid email or password" }, status: 422
+            end
+        else
+            render json: { errors: "Invalid email or password, user nill" }, status: 422
+        end
     end
-  end
 
-  def destroy
-    user = User.find_by_id(params[:id])
-    user.generate_authentication_token!
-    user.save
-    head 204
-  end
+    def destroy
+        user = User.find_by_id(params[:id])
+        user.generate_authentication_token!
+        user.save
+        head 204
+    end
 end
