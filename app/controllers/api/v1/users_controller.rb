1648cac0589bbfaf6bb1cb5c3ba9dfb50c602979
LiamEderzeel <liamederzeel@gmail.com>
2015-10-26 12:54:12 +0100

    user name signup fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -61,6 +61,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation, :klass)
+        params.require(:user).permit(:email, :password, :password_confirmation, :klass, :firstname, :lastname, :prefix)
     end
 end
